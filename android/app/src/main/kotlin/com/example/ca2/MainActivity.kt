97fb05eccf0c0101a088d841a93e933097a97f57
donia2130 <donia99mhamed@gmail.com>
2024-01-30 00:18:23 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/ca2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ca2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Methods/login.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<loginn> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<loginn> {
+  final formKey = GlobalKey<FormState>();
+  final username = TextEditingController();
+  final password = TextEditingController();
+  bool isVisible = false;
+
+  void login() {
+    // Implement your login logic here
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.deepPurple.withOpacity(.2),
+                      // تحديد AssetImage كديكور للصورة
+                      image: const DecorationImage(
+                        image: AssetImage("assets/login.png"),
+                        fit: BoxFit.cover, // اختيار نوع تناسب الصورة
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.deepPurple.withOpacity(.2)),
+                    child: TextFormField(
+                      controller: username,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "username is required";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.person),
+                        border: InputBorder.none,
+                        hintText: "Username",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.deepPurple.withOpacity(.2)),
+                    child: TextFormField(
+                      controller: password,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "password is required";
+                        }
+                        return null;
+                      },
+                      obscureText: !isVisible,
+                      decoration: InputDecoration(
+                          icon: const Icon(Icons.lock),
+                          border: InputBorder.none,
+                          hintText: "Password",
+                          suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  isVisible = !isVisible;
+                                });
+                              },
+                              icon: Icon(isVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off))),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    height: 55,
+                    width: MediaQuery.of(context).size.width * .9,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.deepPurple),
+                    child: TextButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            login();
+                          }
+                        },
+                        child: const Text(
+                          "LOGIN",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
